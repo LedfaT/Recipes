4f721edbc9ae02b3f8175f0b18e84cd27fab6f14
David Zaitsev <davidgalanix@gmail.com>
2025-06-05 16:50:47 +0000

    add main routes for recipes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,16 @@
-import express from "express";
+import express, { Router } from "express";
+import cors from "cors";
 import "dotenv/config";
+import recipeRouter from "./routes/recipeRoutes";
 
 const app = express();
 const port = process.env.PORT || 8080;
+const router = Router();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/api", recipeRouter);
 
 const start = async function () {
   app.listen(port, () => {
---
+++ backend/src/routes/recipeRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const recipeRouter = Router();
+
+// recipeRouter.get("/recipe/list", recipeController.getAllRecipes);
+// recipeRouter.get("/recipe", recipeController.getRecipe);
+
+export default recipeRouter;
